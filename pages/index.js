0220e7e47ac15ca69656833c2186b26e2dfdeb7d
p.capitango65@gmail.com <orlandolaudes7@gmail.com>
2021-07-15 15:28:40 +0100

    Alt....simples

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import styled from "styled-components";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+//const Title = styled.h1`
+//font-size: 50px;
+//color: ${({ theme }) => theme.colors.primary};
+//`;
+const Box = styled.div`
+  background: #ffffff;
+  border-radius: 8px;
 `;
-
 export default function Home() {
-  return <Title>Fuking Bug</Title>;
+  return (
+    <main>
+      <Box>imagem</Box>
+      <Box>bem vindo</Box>
+      <Box>comunidade</Box>{" "}
+    </main>
+  );
 }
